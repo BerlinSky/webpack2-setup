513f33ac3d33604369d385b2a077a371cdf56962
Berlin Sky <pedroyoung8989@gmail.com>
2017-05-16 16:37:41 -0700

    array filter 3

--- app/use-ramda/007-try-catch.js
+++ app/use-ramda/007-try-catch.js
@@ -1,5 +1,32 @@
+import R from 'ramda';
 
+const artist1 = {
+  name: "Ed Sheeran (Also)",
+  DOB:  "Feb 17, 1991",
+  "address1": "101 Country Road",
+  "address2": "Apartment 201",
+  country: "England",
+  genre: "pop",
+  rank: "23",
+  income: "55000000.00"
+}
 
-// R.tryCatch
-// R.propOr
+const justName1 = R.tryCatch(R.prop('name'), R.always('NotFound'));
+
+const justName2 = R.propOr("defaultValue", 'name');
+
+export const getJustArtistName = () => {
+  console.info('justName 1 ========')
+
+  console.log('justName1', justName1(artist1));
+  console.log('justName1', justName1(null));
+  console.log('justName1', justName1(undefined));
+
+  console.info('justName 2 ========')
+  
+  console.log('justName2', justName2(artist1));
+  console.log('justName2', justName2(null));
+  console.log('justName2', justName2(undefined));
+}
+
 
