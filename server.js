cf0af92313e4f95074ea5c80a24d84ca643d8d5c
Berlin Sky <pedroyoung8989@gmail.com>
2017-05-17 18:03:21 -0700

    show users

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 var express = require('express')
 var app = express()
 
+var fs = require('fs')
+var path = require('path');
+
+var users = []
+
+fs.readFile(path.resolve(__dirname, './server/data/users.json'), {encoding: 'utf8'}, function(err, data) {
+  if (err) throw err
+
+  JSON.parse(data).forEach(function(user) {
+    user.name.full = user.name.first + ' ' + user.name.last
+    users.push(user)
+  })
+})
+
 app.get('/', function(req, res) {
-  res.send('Hi, there!!')
+  var buffer = ''
+
+  users.forEach(function(user) {
+    buffer += `<a href="/${user.username}">${user.name.full}</a><br>`
+  })
+
+  res.send(buffer)
+})
+
+app.get('/:username', function(req, res) {
+  var username = req.params.username;
+  res.send(username);
 })
 
 app.get('/service', function(req, res) {
@@ -12,3 +37,4 @@ app.get('/service', function(req, res) {
 var server = app.listen(4000, function() {
   console.log('Server is running at http://localhost:' + server.address().port)
 })
+
