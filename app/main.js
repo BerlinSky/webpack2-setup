26d96b8e0be638365208ba971b88a813815f1a9c
Berlin Sky <pedroyoung8989@gmail.com>
2017-05-23 11:20:01 -0700

    array filter

--- app/main.js
+++ app/main.js
@@ -2,10 +2,12 @@ import $ from 'jquery';
 // import { allRamdaExamples } from './use-ramda/000-all-ramda';
 
 import { getArtistCountryFromList } from './use-js-basic/001-js-map'
+import { getArtistCountryFromList2 } from './use-js-basic/002-js-filter'
 
 $(function () {
   // allRamdaExamples();
 
   console.clear();
   getArtistCountryFromList();
+  getArtistCountryFromList2();
 });
