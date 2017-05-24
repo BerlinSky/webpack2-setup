ecd870b9c47528c29b69f17ad2460137a021424f
Berlin Sky <pedroyoung8989@gmail.com>
2017-05-24 06:31:06 -0700

    for each

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
 // import { allRamdaExamples } from './use-ramda/000-all-ramda';
 
-import { allRamdaExamples } from './use-js-basic/000-all-js-basic'
+import { allJsBacisExamples } from './use-js-basic/000-all-js-basic'
 
 $(function () {
   // allRamdaExamples();
 
   console.clear();
-  allRamdaExamples();
+  allJsBacisExamples();
 });
---
+++ app/use-js-basic/003-js-forEach.js
@@ -0,0 +1,47 @@
+const artists = [
+  {
+    name: "Ed Sheeran",
+    DOB:  "Feb 17, 1991",
+    "address1": "101 Country Road",
+    "address2": "Apartment 201",
+    country: "England",
+    genre: "pop",
+    rank: "23",
+    income: "55000000.00"
+  },
+  {
+    name: "Chrryl Cole",
+    DOB:  "June 30, 1983",
+    "address1": "101 Broadway",
+    "address2": "Studio F",
+    country: "England",
+    genre: "dance-pop",
+    rank: "123",
+    income: "21000000.00"
+  },
+  {
+    name: "Chris Stapleton",
+    DOB:  "April 15, 1978",
+    "address1": "101 Main Street",
+    "address2": "Flat T",
+    country: "USA",
+    genre: "country",
+    rank: "3",
+    income: "19000000.00"
+  }
+]
+
+const getArtistCountry = (artists) => {
+  const result = [];
+
+  artists.forEach(function(artist){
+    result.push(artist.country);
+  })
+
+  return result;
+}
+
+export const getArtistCountryFromList3 = () => {
+  const list = getArtistCountry(artists);
+  console.log('artistCountryList2', list);
+}
