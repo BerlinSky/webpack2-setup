8e5ae40d952c7b3c3598282f5b90092c926ce7c9
Berlin Sky <pedroyoung8989@gmail.com>
2017-07-10 13:27:29 -0700

    updates

--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,4 +1,6 @@
 import { Component, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 @Component({
   selector: 'app',
@@ -7,7 +9,10 @@ import { Component, NgModule } from "@angular/core";
 export class AppComponent{}
 
 @NgModule({
+  imports: [BrowserModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule{}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
