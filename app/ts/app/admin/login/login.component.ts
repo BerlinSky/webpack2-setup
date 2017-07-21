5d1516ffcb6ec4f7bfc5260d2b6d6f076be029dd
Berlin Sky <pedroyoung8989@gmail.com>
2017-07-20 19:26:46 -0700

    updates

--- app/ts/app/admin/login/login.component.ts
+++ app/ts/app/admin/login/login.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  userName = 'ConcertGoer'
+  userName = ''
+  password = ''
 }
