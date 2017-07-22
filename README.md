2eaf6f013720a8ce597772426dc39d3c3e81355a
Berlin Sky <pedroyoung8989@gmail.com>
2017-07-22 13:24:00 -0700

    updates

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## Change Log:
 
-1. Add "register" method to the service
-2. Add "verfyUser" method to the service
-3. Add "login" method to the service
+1. Add signUp method
+2. Add cancel method
 
 
 
--- app/ts/app/admin/signup/signup.component.ts
+++ app/ts/app/admin/signup/signup.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from '../adminShared/user.service'
+
 @Component({
   templateUrl: './signup.component.pug',
    styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  email: string;
+  password1: string;
+  password2: string;
+  passwordFail: boolean = false;
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  signUp() {
+    if (this.password1 !== this.password2) {
+      this.passwordFail = true;
+    }
+    else {
+      this.passwordFail = false;
+      this.userService.register(this.email, this.password1);
+      this.userService.verifyUser();
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['/admin/login']);
+  }
 }
