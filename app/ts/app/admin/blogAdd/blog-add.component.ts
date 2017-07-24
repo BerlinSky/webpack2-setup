f08ccc5cdd8c68f51a6e1abf94cb5ba61f6d886b
Berlin Sky <pedroyoung8989@gmail.com>
2017-07-24 15:12:45 -0700

    updates

---
+++ app/ts/app/admin/blogAdd/blog-add.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BlogAdminService } from '../adminShared/blog-admin.service';
+import { Blog } from '../adminShared/blog';
+
+@Component({
+  selector: 'add-men',
+  templateUrl: './blog-add.component.pug'
+})
+export class BlogAddComponent {
+  imgTitle: string;
+  imageSrc: string;
+  postTitle: string;
+  content: string;
+  post: Blog;
+
+  constructor( private blogAdminService: BlogAdminService, private router: Router ) { }
+
+  fileLoad($event: any) {
+    const thisReader: FileReader = new FileReader();
+    const file: File = $event.target.files[0];
+    this.imgTitle = file.name;
+    thisReader.readAsDataURL(file);
+
+    thisReader.onload = (event: any) => {
+      this.imageSrc = event.target.result;
+    }
+  }
+
+  createPost() {
+    this.post = new Blog (
+      this.postTitle,
+      this.content,
+      this.imgTitle,
+      this.imageSrc.substring(23)
+    );
+
+    this.blogAdminService.createPost(this.post);
+    console.log(`${this.postTitle} added to posts.`);
+    this.router.navigate(['/admin']);
+  }
+
+  cancel() {
+    this.router.navigate(['/admin']);
+  }
+}
