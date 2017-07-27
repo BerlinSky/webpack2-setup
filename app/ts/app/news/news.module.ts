1dc83de7e821d0431cc09d9fc40a1658b5e3dcf9
Berlin Sky <pedroyoung8989@gmail.com>
2017-07-27 16:50:59 -0700

    updates

--- app/ts/app/news/news.module.ts
+++ app/ts/app/news/news.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { NewsComponent } from './news.component';
 import  newsRoutes  from './news.routes';
+import { BlogAdminService } from '../admin/adminShared/blog-admin.service';
 
 @NgModule({
-  imports: [newsRoutes],
+  imports: [CommonModule, newsRoutes],
   declarations: [NewsComponent],
-  exports: [NewsComponent]
+  exports: [NewsComponent],
+  providers: [
+    BlogAdminService
+  ]
 })
 
 export class NewsModule {}
