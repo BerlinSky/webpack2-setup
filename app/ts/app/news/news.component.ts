f8e259ba93d6bef3e2c50b0d5d26c673ddbd8f00
Berlin Sky <pedroyoung8989@gmail.com>
2017-07-26 19:01:50 -0700

    updates

--- app/ts/app/news/news.component.ts
+++ app/ts/app/news/news.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Blog } from '../admin/adminShared/blog';
+import { BlogAdminService } from '../admin/adminShared/blog-admin.service';
 
 @Component({
   selector: 'news-page',
   templateUrl: './news.component.pug',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  blogPosts: Blog[];
+
+  constructor(
+    private blogAdminService: BlogAdminService,
+    private router: Router
+  ) { }
+
+  async ngOnInit() {
+    this.blogPosts = await this.blogAdminService.getPosts();
+
+    console.log('this blogPost??', this.blogPosts);
+  }
+
+  selectPost(post: Blog) {
+    this.router.navigate(['/post', post.id])
+  }
 }
 
